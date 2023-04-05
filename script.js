973b8a0b30f44a7b5a811baf4bf83d736dffadaf
Darren Kim <dkim555@yahoo.com>
2023-04-05 16:48:51 -0700

    create a clearRecipe function

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const newRecipebtn = document.getElementsByClassName('newRecipe');
 const mealContainer = document.getElementById('randomRecipe');
+const recipeName = document.getElementById('recipeName');
+const recipeImage = document.getElementById('recipeImage');
+const recipeIngredients = document.getElementById('ingredientList');
+const recipeInstructions = document.getElementById('stepList');
+const recipeVideo = document.getElementById('recipeVideo');
+const recipeArray = [recipeImage, recipeName, recipeIngredients, recipeVideo, recipeInstructions]
 
 function showRandomRecipe() {
-    const recipeName = document.getElementById('recipeName');
-    const recipeImage = document.getElementById('recipeImage');
-    const recipeIngredients = document.getElementById('ingredientList');
-    const recipeInstructions = document.getElementById('stepList');
-    const recipeVideo = document.getElementById('recipeVideo');
+	axios.get("https://www.themealdb.com/api/json/v1/1/random.php")
+        .then(response => {
+            console.log(response.data.meals[0])
+        })
+}
+
+function clearRecipe() {
+    recipeArray.forEach(element => {
+        element.innerHTML= '';
+    });
 }
