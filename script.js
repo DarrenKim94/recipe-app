fe82240bc2700cfbccb71d8cefdc2a1f096e3b42
Darren Kim <dkim555@yahoo.com>
2023-04-05 14:30:12 -0700

    create variable to grab elements by id

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const newRecipebtn = document.getElementsByClassName('newRecipe');
 const mealContainer = document.getElementById('randomRecipe');
 
-function newRecipe() {
-    axios.get('http://www.themealdb.com/api/json/v1/1/random.php')
-        .then(response => {
-            
-        })
+function showRandomRecipe() {
+    const recipeName = document.getElementById('recipeName');
+    const recipeImage = document.getElementById('recipeImage');
+    const recipeIngredients = document.getElementById('ingredientList');
+    const recipeInstructions = document.getElementById('stepList');
+    const recipeVideo = document.getElementById('recipeVideo');
 }
